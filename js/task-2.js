267b30bd1719234c5bc77a1635aa04f55d08178e
oleksiiMoiseiev89 <alex.moiseiev.ua@gmail.com>
2024-09-07 15:24:59 +0200

    fix task 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,10 @@
 function makeArray(firstArray, secondArray, maxLength){
     const totalArray  = firstArray.concat(secondArray);
-    if(totalArray > maxLength){
-        return maxLength.length;
-    }
-    else {
-        return totalArray.slice(0, maxLength);
-    }
+    if (totalArray.length > maxLength) {
+			return totalArray.slice(0, maxLength);
+		} else {
+			return totalArray;
+		}
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
