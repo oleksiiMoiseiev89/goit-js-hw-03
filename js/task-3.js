73c0973a9facce5721bd53f378d7c0036195f416
oleksiiMoiseiev89 <alex.moiseiev.ua@gmail.com>
2024-09-07 13:54:02 +0200

    home work 3

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+    maxNumberArray = [];
+    for(const number of numbers){
+        if(number > value){
+            maxNumberArray.push(number);
+        }
+    }
+    return maxNumberArray;
+}
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 3 )); // [4, 5] 
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 4 )); // [5] 
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 5 )); // [] 
+console .log(filterArray([ 12 , 24 , 8 , 41 , 76 ], 38 )); // [41, 76] 
+console .log(filterArray([ 12 , 24 , 8 , 41 , 76 ], 20 )); // [24, 41, 76]
